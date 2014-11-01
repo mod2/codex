8378a38b347faa6081c7e346228d9375460f8682
bencrowder <ben.crowder@gmail.com>
2014-10-31 19:46:21 -0600

    Login page styling

--- codex/urls.py
+++ codex/urls.py
@@ -18,7 +18,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', home, name='home'),
     url(r'^account/', include('accounts.urls', namespace='accounts')),
-    url(r'^account/', 'account', name='account'),
+    url(r'^account/', account, name='account'),
     url(r'^admin/login/$', 'accounts.views.login', name='admin:login'),
     url(r'^admin/logout/$', 'accounts.views.logout', name='admin:logout'),
     url(r'^admin/', include(admin.site.urls)),
