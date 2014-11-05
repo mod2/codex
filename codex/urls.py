7724c4736dbe88b73f87bf9dd7a37b4610509f43
chadgh <chadgh@gmail.com>
2014-11-05 15:11:52 -0700

    initial apis for project

--- codex/urls.py
+++ codex/urls.py
@@ -6,21 +6,28 @@ from transcribe.urls import router
 
 @login_required
 def home(request):
+    # TODO: This should probably be in a codex.views file.
     from django.shortcuts import render_to_response
     return render_to_response('index.html')
 
+
 @login_required
 def account(request):
+    # TODO: Shouldn't this be in the accounts apps views?
     from django.shortcuts import render_to_response
     return render_to_response('account.html')
 
+
 @login_required
 def new_project(request):
+    # TODO: Shouldn't this be in the transcribe apps views?
     from django.shortcuts import render_to_response
     return render_to_response('new_project.html')
 
+
 @login_required
 def project(request, project):
+    # TODO: Shouldn't this be in the transcribe apps views?
     from django.shortcuts import render_to_response
     return render_to_response('project.html')
 
@@ -33,12 +40,16 @@ urlpatterns = patterns(
     url(r'^admin/login/$', 'accounts.views.login', name='admin:login'),
     url(r'^admin/logout/$', 'accounts.views.logout', name='admin:logout'),
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^invites/', include('inviter2.urls', namespace='inviter2')),
     url(r'^new-project/', new_project, name='new_project'),
     url(r'^project/(.+?)/', project, name='project'),
     # url(r'^invites/', include('inviter2.urls', namespace='inviter2')),
-    url(r'^transcribe/', include('transcribe.urls', namespace='transcribe')),
-    # url(r'^api/', include(router.urls)),
-    # url(r'^api-auth/', include('rest_framework.urls',
-    #                            namespace='rest_framework')),
 )
+
+transcribepatterns = patterns(
+    '',
+    url(r'^transcribe/api/', include(router.urls)),
+    url(r'^transcribe/api-auth/', include('rest_framework.urls',
+                                          namespace='rest_framework')),
+)
+
+urlpatterns += transcribepatterns
