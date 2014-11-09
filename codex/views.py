a5154a84d564fd4fdf2e131f16ef998283fcb759
bencrowder <ben.crowder@gmail.com>
2014-11-09 14:50:03 -0700

    Dashboard now shows user's projects

--- codex/views.py
+++ codex/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from transcribe.models import Project
 
 @login_required
 def home(request):
-    return render_to_response('index.html')
+    # Get all the user's projects
+    projects = Project.objects.filter(owner=request.user)
+
+    return render_to_response('index.html', { 'projects': projects })
