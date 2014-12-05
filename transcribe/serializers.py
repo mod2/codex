8729015fd277c2581f8f75fb7b18c8842b1be019
chadgh <chadgh@gmail.com>
2014-12-05 09:20:32 -0700

    updated the projects web service from transribe app to include the users that have permission to the project, this also allows the addition and removal of users to/from a project.

--- transcribe/serializers.py
+++ transcribe/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Project
+from accounts.models import User
 
 
 class ProjectSerializer(serializers.ModelSerializer):
+    users = serializers.SlugRelatedField(many=True,
+                                         slug_field='email',
+                                         queryset=(User.objects
+                                                   .filter(is_active=True)))
+
     class Meta:
         model = Project
-        fields = ('id', 'name', 'owner', 'status')
+        fields = ('id', 'name', 'owner', 'status', 'users')
