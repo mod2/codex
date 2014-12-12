708232b43a8d7a83f3f6043ae21dc8e13c10083c
bencrowder <ben.crowder@gmail.com>
2014-12-12 09:01:37 -0700

    Updated local_settings_sample.py

--- local_settings_sample.py
+++ local_settings_sample.py
@@ -1,3 +1,18 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'Put your secret key here. Keep it secret. Keep it safe.'
 
-DEBUG = False
-SECRET_KEY = ''
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Remove the integrations you don't care about
+INTEGRATIONS = {
+    'dropbox': {
+        'key': 'your dropbox key here',
+    },
+    'flickr': {
+        'key': 'your flickr key here',
+        'secret': 'your flickr secret here',
+    },
+}
