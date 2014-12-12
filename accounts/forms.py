3308cab05e7b93f81cafcfc61bba87f9a320948b
chadgh <chadgh@gmail.com>
2014-12-12 13:31:12 -0700

    hooked up account page and created a change password page

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,32 +5,14 @@ import logging
 log = logging.getLogger(__name__)
 
 
-class AccountForm(forms.ModelForm):
+class UserForm(forms.ModelForm):
     """
     Form for the user account information/preferences.
     """
-    password1 = forms.CharField(widget=forms.widgets.PasswordInput,
-                                label='Password')
-    password2 = forms.CharField(widget=forms.widgets.PasswordInput,
-                                label='Password (again)')
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
-
-    def save(self, commit=True):
-        user = super(AccountForm, self).save(commit=False)
-        user.set_password(self.cleaned_data['password1'])
-        if commit:
-            user.save()
-        return user
-
     class Meta:
         model = User
-        fields = ['email', 'password1', 'password2']
+        fields = ['email', 'name', 'layout', 'theme']
+        readonly_fields = ['email']
 
 
 class AuthenticationForm(forms.Form):
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,7 @@
+from __future__ import unicode_literals
+
 from django.conf.urls import url, include
+from django.contrib.auth.views import password_change
 from rest_framework.routers import SimpleRouter
 
 from .views import UserViewSet
@@ -11,5 +14,11 @@ urlpatterns = [
     url(r'^logout/$', 'accounts.views.logout', name='logout'),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
-                               namespace='accounts_rest_framework'))
+                               namespace='accounts_rest_framework')),
+    url(r'^password/saved/$', 'accounts.views.password_saved',
+        name='password_saved'),
+    url(r'^password/change/$', password_change,
+        {'template_name': 'accounts/change_password.html',
+         'post_change_redirect': 'accounts:password_saved'},
+        name='password_change'),
 ]
